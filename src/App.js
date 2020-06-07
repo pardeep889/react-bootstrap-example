c9bdfa3043675a4b4292d387e39a30b2ee0ad241
pardeep-mern <pardeep889@hotmail.com>
2020-06-07 17:27:18 +0530

    boiler plate

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header/header";
+import SectionOne from "./Sections/SectionOne";
+
+import Footer from "./Footer/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <SectionOne />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sections/SectionOne.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SectionOne = () => {
+    return (
+        <div>
+            Section One
+        </div>
+    );
+};
+
+export default SectionOne;
